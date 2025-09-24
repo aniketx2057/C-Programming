9de4a8cbc5340b019ed259123389944879838dc8
Aniket Kumar <www.aniketkingmaker@gmail.com>
2025-09-24 16:08:25 +0530

    Add files via upload

---
+++ value of an integer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+   int a,z;
+   printf("Enter the integer: ");
+   scanf("%d",&a);
+   if (a>0)
+   z=1;
+   else
+   if(a == 0)
+   z=0;
+   else
+   z=-1;
+   printf("n is %d\z",z);
+   return 0;
+
+}
